a948d46c4bd2faf69070caec648f26389939758a
Nandnigarg <gargnandni64@gmail.com>
2023-03-01 21:50:53 +0530

    Sainik Images

--- src/components/Other comps/Aissee.js
+++ src/components/Other comps/Aissee.js
@@ -1,7 +1,28 @@
+import S1 from '../assets/Sainik-1.jpg';
+import S2 from '../assets/Sainik-2.jpg';
+import S3 from '../assets/Sainik-3.jpg';
+import S4 from '../assets/Sainik-4.jpg';
+import S5 from '../assets/Sainik-5.jpg';
+import S6 from '../assets/Sainik-6.jpg';
+import S7 from '../assets/Sainik-7.jpg';
+import S8 from '../assets/Sainik-8.jpg';
+import S9 from '../assets/Sainik-9.jpg';
+import S10 from '../assets/Sainik-10.jpg';
+import S11 from '../assets/Sainik-11.jpg';
+import S12 from '../assets/Sainik-12.jpg';
+import S13 from '../assets/Sainik-13.jpg';
+import S14 from '../assets/Sainik-14.jpg';
+import S15 from '../assets/Sainik-15.jpg';
+import S16 from '../assets/Sainik-16.jpg';
+import S17 from '../assets/Sainik-17.jpg';
+import S18 from '../assets/Sainik-18.jpg';
+import S19 from '../assets/Sainik-19.jpg';
+import S20 from '../assets/Sainik-20.jpg';
+
 function Aissee() {
     return (
         <div className="container-fluid" style={{ paddingLeft: "0", paddingRight: "0" }}>
-            <div className="container mt-5 mb-5">
+            <div className="container mt-5 mb-4">
                 <h1 style={{ fontFamily: "cursive", fontSize: "3rem", fontWeight: "bold", textAlign: "center" }} className="text text-danger">Sainik School - AISSEE</h1>
                 <hr />
                 <br /><br />
@@ -20,6 +41,76 @@ function Aissee() {
                 <h3 style={{ color: "rgb(21, 52, 98)", fontSize: "1.7rem", fontWeight: "bold" }}>Real-time Environment:</h3>
                 <p style={{ fontSize: "18px" }}>We provide a real-time environment for SSB interview preparation, which helps our students get a feel for the actual interview. This experience gives them an edge over others who may not have had similar exposure.</p>
                 <p style={{ fontSize: "18px" }}>In conclusion, DCG Defence Academy is the best choice for Sainik School Entrance Exam preparation. Our expert coaching, flexible learning options, motivational sessions, specialized study material, interview skill development, and real-time environment make us the go-to institute for aspirants who want to succeed in their defense careers.</p>
+                <div className="row mt-5">
+                    <div className="col-md-6">
+                        <iframe width="100%" height="400" src="https://www.youtube.com/embed/NMM4A5V6AUI" title="Sainik School 2023 Result Celebration | Delhi Career Group | #sainikschool #result #viral #shorts" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div className="col-md-6">
+                        <iframe width="100%" height="400" src="https://www.youtube.com/embed/4sIbchkPOvY" title="Sainik School 2023 Achievers &amp; Parents Feedback | Achievers Experience | Topper |  #dcg #result" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S1} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S2} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S3} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S4} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S5} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S6} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S7} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S8} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S9} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S10} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S11} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S12} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S13} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S14} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S15} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S16} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S17} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S18} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S19} alt="" height="auto" width="100%" />
+                    </div>
+                    <div className="col-md-3 mt-3 p-2">
+                        <img src={S20} alt="" height="auto" width="100%" />
+                    </div>
+                </div>
             </div>
         </div>
     )
