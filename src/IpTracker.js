2ef9575fa685921dfedbb4eaf2496a6e92c4fc2f
Nandnigarg <gargnandni64@gmail.com>
2023-05-12 16:02:14 +0530

    IP Added to sheets

--- src/IpTracker.js
+++ src/IpTracker.js
@@ -9,22 +9,18 @@ function IpTracker() {
         const res = await axios.get("https://api.ipgeolocation.io/ipgeo?apiKey=069668300aa24a0ab1c7e950dc0be94a");
         console.log(res.data);
         setIP(res.data.ip);
-        var form = document.getElementById('sheetdb-form');
-        fetch("https://sheetdb.io/api/v1/vhc35dylluas0", {
-            method: "POST",
-            body: new FormData(document.getElementById("sheetdb-form")),
-        }).then((html) => {
-            console.log("ip sent successfully");
-        });
+        console.log(IP);
+        axios.post("https://sheet.best/api/sheets/e2f16ba1-26af-4ea7-af7c-eaa60cb7bdc2", {
+            "IP": IP
+        })
+            .then((html) => {
+                console.log("ip sent successfully");
+            });
     };
-
-    useEffect(() => { getData(); }, []);
+    useEffect(() => { getData(); });
 
     return (
-        <form action="https://sheetdb.io/api/v1/vhc35dylluas0" method="POST" id="sheetdb-form">
-            <input name="data[ip]" value={IP} hidden/>
-            <div>{console.log[IP]}</div>
-        </form>
+        <div>{console.log[IP]}</div>
     )
 }
 
