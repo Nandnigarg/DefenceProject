fd0be6cf573c21410081463f2c5e8d3bcdaf30df
Nandnigarg <gargnandni64@gmail.com>
2023-06-16 01:35:06 +0530

    Changes 29

--- src/components/Other comps/NdaCoaching.js
+++ src/components/Other comps/NdaCoaching.js
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 import "../Structure/glow.css";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
-function NdaCoaching(){
-    return(
+function NdaCoaching() {
+    return (
         <div className="container-fluid" style={{ paddingLeft: "0", paddingRight: "0" }}>
             <Helmet>
                 <title>NDA Coaching in Chandigarh | NDA Coaching in Delhi - Delhi Career Group</title>
@@ -12,15 +12,96 @@ function NdaCoaching(){
             <div className="container mt-5 mb-5" style={{ textAlign: "justify" }}>
                 <h1 style={{ fontSize: "3rem", fontWeight: "bold", textAlign: "center" }} className="text text-danger">NDA Coaching</h1>
                 <hr /><br />
-                <div style={{textAlign: "center"}}><iframe width="80%" height="400" src="https://www.youtube.com/embed/l_BiGgcGeiQ" title="How to Crack NDA Exam in First Attempt! | Mr. Vatsom  - DCG Academy" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
                 <p style={{ fontSize: "18px" }}>DCG understands the needs of every student and has designed various NDA coaching programs to suit the needs of NDA  aspirants. We offer NDA coaching to our students in both online and offline modes.</p>
                 <p style={{ fontSize: "18px" }}>Our online portal provides NDA classes in two formats: NDA Online live interactive classes and NDA Online recorded lectures.  Offline  mode students can attend our NDA classes at our NDA coaching centers in Delhi and Chandigarh.</p>
                 <p style={{ fontSize: "18px" }}>Our online NDA coaching program is designed to be convenient and flexible for students who want to study for NDA from the comfort of their own homes. Our NDA online live interactive classes allow students to interact with our experienced faculty in real time. While  our NDA recorded lectures can be accessed at any time.</p>
                 <p style={{ fontSize: "18px" }}>Our NDA offline coaching program is designed for students who prefer to study in a traditional classroom setting with more focus and discipline. Our experienced faculty will provide you with the knowledge and skills you need to succeed on the NDA exam.</p>
                 <p style={{ fontSize: "18px" }}>No matter which mode of NDA coaching you choose, DCG can help you achieve your dream of becoming a commissioned officer in the Indian Armed Forces</p>
-                <div style={{borderRadius: "20px", backgroundColor: "rgb(40,120,70)" }} className='p-4'>
+                <div className='row'>
+                    <div className='col-sm-6' style={{ textAlign: "center" }}><iframe width="95%" height="380" src="https://www.youtube.com/embed/l_BiGgcGeiQ" title="How to Crack NDA Exam in First Attempt! | Mr. Vatsom  - DCG Academy" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+                    <div className='col-sm-6' style={{ textAlign: "center" }}><iframe width="95%" height="380" src="https://www.youtube.com/embed/sU0vOD4fkQA" title="Delhi Career Group  &quot;A Network of Academic Excellence&quot; | Students Feedback| Academy Tour." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+                </div>
+                <br />
+                <div style={{ textAlign: "center" }}>
+                    <h3>(ONLINE & OFFLINE CLASSES)</h3>
+                </div><br />
+                <p style={{ fontSize: "18px" }}>With the best coaching experience for our students, our NDA coaching centers in Chandigarh and Delhi provide cutting-edge facilities and the most recent teaching tools. To make sure that our students are adequately prepared for all components of the NDA exam, we also put a strong emphasis on interview skills in addition to written exam preparation.</p>
+                <h2><u>Eligibility Criteria</u></h2>
+                <p style={{ fontSize: "18px" }}>The eligibility criteria for NDA are as follows:</p>
+                <ol style={{ fontSize: "18px" }}>
+                    <li><b>Nationality:</b> According to NDA eligibility, an applicant who is an Indian citizen is eligible to apply for the upcoming examination. Those who are not a citizen of India and still want to apply for the exam must meet the UPSC NDA eligibility criteria listed below:</li>
+                    <p style={{ fontSize: "18px" }}>The candidate applying for the exam must be either:</p>
+                    <ol style={{ fontSize: "18px" }}>
+                        <li>A Citizen of India</li>
+                        <li>A Subject of Nepal or</li>
+                        <li>A subject of Bhutan, or,</li>
+                        <li>Tibetan refugees who came to India before 1 Jan. 1962, with the intention of permanent settlement.</li>
+                        <li>A person of Indian origin who migrated from the following countries with the intention of permanent settlement:
+                            <ul>
+                                <li>Pakistan, Sri Lanka</li>
+                                <li>East African Countries of Kenya, Uganda, the United Republic of Tanzania, Zambia, Malawi, Zaire & Ethiopia, and Vietnam.</li>
+                            </ul>
+                        </li>
+                    </ol>
+                    <li><b>Age Limit:</b> Candidates must be between 16.5 and 19.5 years of age at the time of commencement of the course. The age limits are different for different branches:</li>
+                    <ul>
+                        <li>Army: Candidates must be between 16.5 and 19.5 years.</li>
+                        <li>Air Force and Navy: Candidates must be between 16.5 and 19.5 years.</li>
+                    </ul>
+                    <li><b>Gender:</b> Male and Female candidates are eligible for NDA.</li>
+                    <li><b>Qualifications:</b></li>
+                    <ul>
+                        <li>Army Wing: Candidates must have completed their 10+2 pattern of school education or equivalent examination conducted by a state education board or a university.</li>
+                        <li>Air Force and Naval Wings: Candidates must have completed their 10+2 pattern of school education or equivalent with Physics and Mathematics as compulsory subjects, conducted by a state education board or a university.</li>
+                    </ul>
+                    <li><b>Note:</b> Candidates appearing in the 10+2 examination are also eligible to apply, provided they meet the above educational qualifications at the time of joining the course.</li>
+                    <li><b>Marital Status:</b> Candidates must be unmarried. Marriage is not permitted during the training period at NDA.</li>
+                </ol>
+                <h2><u>Exam Pattern</u></h2>
+                <p style={{ fontSize: "18px" }}>The written exam for the National Defence Academy (NDA) consists of two papers: Mathematics and General Ability Test (GAT). The exam pattern for NDA written exam is as follows:</p>
+                <p style={{ fontSize: "18px" }}>Paper 1: Mathematics</p>
+                <ul style={{ fontSize: "18px" }}>
+                    <li>Duration: 2.5 hours</li>
+                    <li>Maximum Marks: 300</li>
+                    <li>Questions: Multiple Choice Questions (MCQs)</li>
+                    <li>Syllabus: The mathematics paper covers topics such as Algebra, Matrices and Determinants, Trigonometry, Analytical Geometry, Differential Calculus, Integral Calculus and Differential Equations, Vector Algebra, Statistics, and Probability.</li>
+                </ul>
+                <p style={{ fontSize: "18px" }}>Paper 2: General Ability Test (GAT)</p>
+                <ul style={{ fontSize: "18px" }}>
+                    <li>Duration: 2.5 hours</li>
+                    <li>Maximum Marks: 600</li>
+                    <li>Questions: Multiple Choice Questions (MCQs)</li>
+                    <li>Syllabus: The GAT paper is further divided into two sections: English and General Knowledge.</li>
+                    <ul>
+                        <li>English: The English section tests candidates' understanding of English grammar, vocabulary, comprehension, and basic communication skills.</li>
+                        <li>General Knowledge: The General Knowledge section covers various topics such as Physics, Chemistry, General Science, History, Geography, Current Events, and Social Studies.</li>
+                    </ul>
+                </ul>
+                <p style={{ fontSize: "18px" }}>Both papers are conducted on the same day, and each paper is assigned a maximum of 300 marks. The total marks for the written exam are 900 (300 marks for Mathematics + 600 marks for GAT).</p>
+                <p style={{ fontSize: "18px" }}>It's important to note that there is a negative marking in the NDA written exam. For every incorrect answer, one-third (0.33) of the marks allotted to that question are deducted as a penalty.</p>
+                <p style={{ fontSize: "18px" }}>Candidates who qualify for the written exam are then called for the next stages of the selection process, which include the Service Selection Board (SSB) interview, psychological aptitude test, medical examination, and final merit list preparation.</p>
+                <h2><u>Selection process :</u></h2>
+                <ul style={{ fontSize: "18px" }}>
+                    <li>Writter Exam</li>
+                    <li>SSB Selecetion Test</li>
+                    <li>Medical Examination</li>
+                </ul>
+                <br />
+                <h3><u>Written Exam</u></h3>
+                <p style={{ fontSize: "18px" }}>The written exam for the National Defence Academy is broken into two sections: </p>
+                <ul style={{ fontSize: "18px" }}>
+                    <li>Mathematics</li>
+                    <li>General ability</li>
+                </ul>
+                <p style={{ fontSize: "18px" }}>The written exam has a total of 900 marks because the maths exam carries 300 points and the general ability exam carries 600 points. The General Ability exam covers maths and reasoning, as well as current events, physics, chemistry, history, and English.</p>
+                <h2><u>SSB Round</u></h2>
+                <p style={{ fontSize: "18px" }}>After passing the written exam, candidates must proceed to the SSB phase, which entails a general conference, a basic cognitive test, a psychological examination, and group testing. The duration of this lengthy test is five days.</p>
+                <h2><u>Medical Test</u></h2>
+                <p style={{ fontSize: "18px" }}>The candidate must show up for a medical exam after passing the SSB stage of qualification. When an applicant passes the medical examination, the Union Public Service Commission declares him to be a qualified candidate. On the UPSC website, the complete list of candidates who passed is available.</p>
+                <br />
+                <div style={{ borderRadius: "20px", backgroundColor: "rgb(40,120,70)" }} className='p-4'>
                     <h3 style={{ color: "rgb(255,255,0)", fontSize: "2.7rem", textAlign: "center" }}>NDA Coaching Programs Offered by Delhi Career Group</h3>
-                    <div className="row mb-5" style={{textAlign: "center"}}>
+                    <div className="row mb-5" style={{ textAlign: "center" }}>
                         <div className="col-sm-4 mt-4">
                             <Link exact to="/nda-online" onClick={() => { window.scroll({ top: 0, left: 0, behavior: "smooth" }); }}><button className='btn btn-lg glow-btn' style={{ backgroundColor: "rgb(178,0,0)", color: "rgb(255,255,0)", border: "none" }}>NDA Online Coaching</button></Link>
                         </div>
